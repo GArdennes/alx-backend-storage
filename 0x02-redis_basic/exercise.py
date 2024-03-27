3a748972017901cf4d33c1a1f85da9f84aa50c86
GArdennes <kevin.afachao@gmail.com>
2024-03-27 14:03:16 -0700

    Task 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -80,7 +80,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
-    @call_history 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
