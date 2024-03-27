5cc1cfc94b7ec5a0754ab073389da28e0b06c36d
GArdennes <kevin.afachao@gmail.com>
2024-03-27 13:46:47 -0700

    Task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -36,6 +36,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores data in the cache and returns the generated key
